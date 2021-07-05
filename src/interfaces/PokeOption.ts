85212ebe2115f7e80c085b0e1627bb7f84a4c13a
Lawson Cheng <inbox@lawsoncheng.com>
2021-07-06 00:34:35 +0800

    Added explicit possible values for PokeOption methods

--- src/interfaces/PokeOption.ts
+++ src/interfaces/PokeOption.ts
@@ -1,5 +1,5 @@
 export default interface PokeOption {
-    method? : string
+    method? : 'CREATE'|'GET'|'PUT'|'DELETE'|'PATCH'|'OPTIONS'|'HEAD'|'CONNECT'|'TRACE',
     path? : string
     port? : number
     headers? : Headers
