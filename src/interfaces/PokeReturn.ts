8add2ae4a8168c8481e34a3322202ceef32a278e
Lawson Cheng <inbox@lawsoncheng.com>
2021-07-05 18:21:49 +0800

    Added 3 event listeners to PokeReturns in order to support stream related functions

--- src/interfaces/PokeReturn.ts
+++ src/interfaces/PokeReturn.ts
@@ -1,7 +1,13 @@
 import PokeResult from './PokeResult'
+import * as http from 'http'
 
 export default interface PokeReturn {
+    req: http.ClientRequest|undefined,
     promise : () => Promise<PokeResult>,
     // abort request
-    abort : () => void
+    abort : () => void,
+    // event listeners
+    on: (eventName:'data'|'error'|'response', callback:(result:any) => void) => void,
+    /* ----- stream ----- */
+    pipe: (chunck:string) => void,
 }
