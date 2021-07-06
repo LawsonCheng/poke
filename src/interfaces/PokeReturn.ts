86b44903873bfc278ba3f0142ead325072032bd0
Lawson Cheng <inbox@lawsoncheng.com>
2021-07-07 00:04:17 +0800

    Added new event 'response' to the 'on' method

--- src/interfaces/PokeReturn.ts
+++ src/interfaces/PokeReturn.ts
@@ -9,7 +9,7 @@ export default interface PokeReturn {
     // abort request
     abort : () => void,
     // event listeners
-    on: (eventName:'data'|'error'|'response'|'end', callback:(result:any) => void) => void,
+    on: (eventName:'data'|'error'|'response'|'end', callback:(result?:any) => void) => PokeReturn,
     /* ----- stream ----- */
     pipe: (writableStream:WriteStream) => void,
 }
