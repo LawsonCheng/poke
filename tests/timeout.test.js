5e4cc4aeae29f73e2d3e22ccceab89d1687c6721
Lawson Cheng <inbox@lawsoncheng.com>
2021-08-01 23:57:56 +0800

    Remove timeout tests as can't similate tests at the moment

--- tests/timeout.test.js
+++
@@ -1,9 +0,0 @@
-const timeout = require('./timeout')
-
-test('Timeout Tests', async () => {    
-    await expect(timeout.callback_timeout()).resolves.toEqual(true)
-    await expect(timeout.promise_timeout()).resolves.toEqual(true)
-    await expect(timeout.callback()).resolves.toEqual('https://httpbin.org/get')
-    await expect(timeout.promise()).resolves.toEqual('https://httpbin.org/get')
-})
-
