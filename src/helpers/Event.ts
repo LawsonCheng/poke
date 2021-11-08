0da57c0d1d07ebea149cbdfb4f9d4a87e7ddcf03
Lawson Cheng <inbox@lawsoncheng.com>
2021-11-08 17:27:40 +0800

    Removed legacy method

--- src/helpers/Event.ts
+++ src/helpers/Event.ts
@@ -2,9 +2,10 @@ import { WriteStream } from 'fs'
 import { ServerResponse } from 'http'
 import PokeResult, { PokeError, PokeSuccess } from '../interfaces/PokeResult'
 
-
+/**
+ * Defines protocol js.Poke supports
+ */
 type Protocol = 'http' | 'https';
-
 
 /**
  * Callback event name
@@ -12,20 +13,13 @@ type Protocol = 'http' | 'https';
 type CallbackEvent = 'data' | 'error' | 'response' | 'end'
 
 /**
- * 
- * Determines whether the event name is a valid callback event
- * @param input:string
- * @returns boolean
+ * Defines types of stream
  */
-
-
-function isCallbackEvent(input:string): input is CallbackEvent {
-    return /^data|error|response|end$/.test(input)
-}
-
-
 type Stream = WriteStream | ServerResponse
 
+/**
+ * Defines container for different callbacks
+ */
 type EventCallbacksContainer = {
     [e in CallbackEvent | 'stream']? : 
         e extends 'data' ? (chunk:string) => void : 
@@ -35,25 +29,9 @@ type EventCallbacksContainer = {
         e extends 'stream' ? Stream : never
 }
 
-/**
- * EventManager
- * Stores method that handles difference events
- */
-
-
-interface EventManager {
-    set: <Event extends CallbackEvent>(eventName: Event, callback: EventCallbacksContainer[Event]) => void,
-    response: NonNullable<EventCallbacksContainer['response']>,
-    end: NonNullable<EventCallbacksContainer['end']>,
-    error: NonNullable<EventCallbacksContainer['error']>,
-    data: NonNullable<EventCallbacksContainer['data']>,
-    stream: (writableStream: Stream) => void
-}
-
 export class EventManagerClass {
     callbacks: EventCallbacksContainer; 
     isPokeError: (input:PokeError) => void;  
-    // isProtocol: (input: Protocol) => void; 
 
     constructor(){
         this.callbacks = {}
@@ -111,69 +89,7 @@ export class EventManagerClass {
     }
 
     stream(writableStream: Stream):void {
-        this.callbacks['stream'] = writableStream   // save stream
+        // save stream
+        this.callbacks['stream'] = writableStream   
     }
 }
-
-
-/**
- * Initiate and return an EventManager
- * @returns EventManager
- */
-
-const initEventManager = function (): EventManager {
-    // the place to stores those callbacks
-    const callbacks: EventCallbacksContainer = {}   // return append callback methods and event callback methods
-    return {
-        // set 
-        set: (eventName, callback) => {
-            // valid event name and callback is a function
-            if(isCallbackEvent(eventName)) {
-                callbacks[eventName] = callback // assign listener to listeners container
-            }
-        },
-
-
-        response: (result) => {
-            if(callbacks['response'] !== undefined) {
-                // return response
-                callbacks['response'](result)
-            }
-        },
-        end: () => {
-            if(callbacks['end'] !== undefined) {
-                // emit end event
-                callbacks['end']()
-            }
-            // ensure stream exists
-            if(callbacks['stream'] !== undefined) {
-                // emit stream end event
-                callbacks['stream'].end()
-            }
-        },
-        error: (result) => {
-            if(callbacks['error'] !== undefined) {
-                // return response object with error
-                callbacks['error'](result)
-            }
-        },
-
-
-        data: (chunk) => {
-            if(callbacks['data'] !== undefined) {
-                callbacks['data'](chunk)
-            }
-            // ensure stream exists
-            if(callbacks['stream'] !== undefined) {
-                // emit stream end event
-                callbacks['stream'].write(chunk)
-            }
-        },
-        stream: (writableStream) => {
-            // save stream
-            callbacks['stream'] = writableStream
-        }
-    }
-}
-
-export default initEventManager
